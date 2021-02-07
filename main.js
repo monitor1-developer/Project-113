a5cc4f5bdbfca802bfd58e8d96c40e424e5d7e73
monitor1-developer <76256724+monitor1-developer@users.noreply.github.com>
2021-02-07 11:08:25 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+function preload() {
+	
+}
+
+function setup() {
+	canvas = createCanvas(640, 480);
+	canvas.position(110, 250);
+	video = createCapture(VIDEO);
+	video.hide();
+}
+
+function draw() {
+	image(video, 230, 150, 220, 200);
+	
+	
+	
+
+	fill(0, 128, 0);
+	stroke(0, 128, 0);
+	rect(90, 40, 460, 20);
+	rect(90, 420, 460, 20);
+	rect(40, 40, 20, 350);
+	rect(570, 40, 20, 350);
+
+	fill(255, 0, 0);
+	stroke(255, 0, 0);
+	circle(50, 50, 80);
+	circle(50, 430, 80);
+	circle(580, 430, 80);
+	circle(580, 50, 80);
+
+}
+
+function take_snapshot() {
+	save('danny.png');
+}
